d7643da47394e6a92a62ed1789e7a786631bbd08
devid <nnick4673@gmail.com>
2022-09-29 08:10:34 -0300

    Aplicação refeitorio adicionada ao arquivo de configurações

--- src/settings.py
+++ src/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'refeitorio.apps.RefeitorioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
