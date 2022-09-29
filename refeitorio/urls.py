4213c5ecadae5eca95972ca1911b9b3e5cfe6135
thidoa <thiagovini200@gmail.com>
2022-09-29 16:24:48 -0300

    crei o front para teste

--- refeitorio/urls.py
+++ refeitorio/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('login_aluno/', views.login_aluno, name ='login_aluno'),
-    path('login_funcionario', views.login_funcionario, name='login_funcionario')
+    path('login_aluno/', views.login_aluno, name='login_aluno'),
+    path('login_funcionario/', views.login_funcionario, name='login_funcionario')
 ]
--- src/settings.py
+++ src/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
