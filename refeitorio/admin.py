137059e2bc86fc652cd33fd68b6fa206cbe070b2
devid <nnick4673@gmail.com>
2022-09-29 15:42:46 -0300

    Adicionei os funcionarios ao painel administrativo do django

--- refeitorio/admin.py
+++ refeitorio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Aluno
+from .models import Aluno, Funcionario
 
 # Register your models here.
 
 admin.site.register(Aluno)
+admin.site.register(Funcionario)
