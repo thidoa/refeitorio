87c39363aadd2595813a6943ae46785b8aa1ea84
devid <nnick4673@gmail.com>
2022-09-30 12:33:30 -0300

    Implementada a lógica do backend das telas de registro

--- refeitorio/views.py
+++ refeitorio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Aluno, Funcionario
 from .forms import AlunoLogin, FuncionarioLogin, AlunoRegister, FuncionarioRegister
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
@@ -33,7 +34,17 @@ def register_aluno(request):
     context = {}
 
     if request.method == 'POST':
-        pass
+        nome = request.POST['nome']
+        matricula = request.POST['matricula']
+        senha = request.POST['senha']
+        quentinha = request.POST['quentinha']
+
+        password_hash = make_password(password=senha, hasher='argon2')
+
+        aluno = Aluno(nome=nome, matricula=matricula, quentinha=quentinha, senha=password_hash)
+        aluno.save()
+
+        return render(request, 'index.html')
     else:
         form = AlunoRegister()
         context['form'] = form
@@ -44,7 +55,16 @@ def register_funcionario(request):
     context = {}
 
     if request.method == 'POST':
-        pass 
+        nome = request.POST['nome']
+        siap = request.POST['siap']
+        senha = request.POST['senha']
+        
+        password_hash = make_password(password=senha, hasher='argon2')
+
+        funcionario = Funcionario(nome=nome, siap=siap, senha=password_hash)
+        funcionario.save()
+
+        return render(request, 'index.html')
     else:
         form = FuncionarioRegister()
         context['form'] = form
