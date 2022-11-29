f83d7370b86925ece7a85a9f3554d410e7dc9b89
thidoa <thiagovini200@gmail.com>
2022-11-29 08:21:39 -0300

    testes

--- refeitorio/views.py
+++ refeitorio/views.py
@@ -6,7 +6,6 @@ from .forms import AlunoLogin, FuncionarioLogin, AlunoRegister, FuncionarioRegis
 from datetime import datetime
 from pytz import timezone
 import calendar
-import datetime
 
 # Create your views here.
 
@@ -142,44 +141,108 @@ def register_funcionario(request):
     return render(request, 'register_funcionario.html', context)
 
 def teste(request):
-    data = datetime.datetime.now()
 
-    dia_mes_atual = []
-    dia_calendario = []
+    usuario = request.user
+    if hasattr(usuario, 'aluno'):
+        aluno = Aluno.objects.get(id=usuario.id)
+        if request.method == 'POST':
+            dias = request.POST.getlist('dias_da_semana')
 
-    obj = calendar.Calendar(firstweekday = 6)
-    for day in obj.itermonthdays(data.year, data.month):
-        dia_mes_atual.append(day)
-        #print(day)
+            for dia in aluno.quentinha.keys():
+                if dia in dias:
+                    aluno.quentinha[dia] = '1'
+                else:
+                    aluno.quentinha[dia] = '0'
 
-    data_mes = calendar.Calendar(firstweekday = 6)
-    for dia in data_mes.itermonthdates(data.year, data.month):
-        data_dia = str(dia).split('-')
-        dia_calendario.append(data_dia[-1])
+            aluno.save(update_fields=['quentinha'])    
+            return redirect('/')
 
-    mes_completo = []
 
-    for valor in range(len(dia_mes_atual)):
-        mes_completo.append([dia_mes_atual[valor], dia_calendario[valor]])
-   
-    dias_semana = []
-    semana_mes =[]
-    contador = 0
-    for dia in dia_calendario:
-        if contador < 6:
-            dias_semana.append(dia)
-            contador+=1
-        else:
-            dias_semana.append(dia)
-            semana_mes.append(dias_semana)
-            dias_semana = []
-            contador = 0
-    
-    context = {
-        'dia_mes_atual': dia_mes_atual,
-        'mes': semana_mes,
-        'agora_vai': mes_completo,
-    }
+        data = datetime.now()
+
+        dia_atual = data.day
+
+        dia_mes_atual = []
+        dia_calendario = []
+
+        obj = calendar.Calendar(firstweekday = 6)
+        for day in obj.itermonthdays(data.year, data.month):
+            dia_mes_atual.append(day)
+
+        data_mes = calendar.Calendar(firstweekday = 6)
+        for dia in data_mes.itermonthdates(data.year, data.month):
+            data_dia = str(dia).split('-')
+            dia_calendario.append(data_dia[-1])
+
+        mes_completo = []
+
+        mescompleto2 = []
+
+        for valor in range(len(dia_mes_atual)):
+            mes_completo.append([dia_mes_atual[valor], dia_calendario[valor]])
+
+
+        dias_semana2 = []
+        semana_mes2 =[]
+        contador2 = 0
+        for dia in mes_completo:
+            if contador2 < 6:
+                dias_semana2.append(dia)
+                contador2+=1
+            else:
+                dias_semana2.append(dia)
+                semana_mes2.append(dias_semana2)
+                dias_semana2 = []
+                contador2 = 0
+
+
+        dias_semana = []
+        semana_mes =[]
+        contador = 0
+        for dia in dia_calendario:
+            if contador < 6:
+                dias_semana.append(dia)
+                contador+=1
+            else:
+                dias_semana.append(dia)
+                semana_mes.append(dias_semana)
+                dias_semana = []
+                contador = 0
+
+        semana_atual = 0
+        for i in range(len(semana_mes)):
+            if str(dia_atual) in semana_mes[i]:
+                semana_atual = semana_mes[i]
+
+        semana_quentinha = []
+        for i in range(len(semana_atual)):
+            if i != 0 and i != len(semana_atual):
+                semana_quentinha.append(semana_atual[i])
+
+        teste = []
+        lista_menor = []
+        c = 0
+        for quentinha in aluno.quentinha:
+            lista_menor = [semana_quentinha[c] ,aluno.quentinha[quentinha]]
+            teste.append(lista_menor)
+            c += 1
+
+        print(teste)
+
+        context = {
+            'dia_mes_atual': dia_mes_atual,
+            'mes': semana_mes,
+            'agora_vai': mes_completo,
+            'mes2': semana_mes2,
+            'dia_atual': dia_atual,
+            'semana_atual': semana_atual,
+            "quentinhas": aluno.quentinha,
+            'teste': teste,
+        }
+
+        print(semana_atual)
+
+
     return render(request, 'teste.html', context)
 
 def faltas(request):
